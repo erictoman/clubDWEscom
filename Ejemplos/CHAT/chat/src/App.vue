0426c09c7b09014ade086a537a482807bf03d453
erictoman <ericto117@gmail.com>
2020-03-04 14:34:20 -0600

    Chat

---
+++ Ejemplos/CHAT/chat/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <entrada />
+  </div>
+</template>
+
+<script>
+import entrada from "./components/entrada";
+export default {
+  name: "App",
+  components: { entrada }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Ejemplos/CHAT/chat/src/components/entrada.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="modal" :class="{ 'is-active': modalActivo }">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Registro</p>
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">Usuario:</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <p class="control">
+                  <input
+                    v-model="nombre"
+                    class="input is-info"
+                    type="text"
+                    placeholder="Juan Perez"
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button @click="this.accept" class="button is-info">Aceptar</button>
+          <button @click="this.close" class="button is-danger">Cancelar</button>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      modalActivo: true,
+      nombre: ""
+    };
+  },
+  methods: {
+    close() {
+      this.modalActivo = false;
+    },
+    accept() {
+      this.$emit("registrar", this.nombre);
+    }
+  }
+};
+</script>
